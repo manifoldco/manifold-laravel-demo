ab15cf608b7f74a9ce77f2ede67e276f15fc8451
plaroche <patrick@manifold.co>
2017-12-13 15:10:57 -0400

    Added Logging
    - greatly improved the readme as well

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -34,6 +35,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
+        Log::info('User Loging in');
         $this->middleware('guest')->except('logout');
     }
 }
